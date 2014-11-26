ce06a4e997a6d49bdf058f1780749854db826f23
gtskk <tttt6399998@126.com>
2014-11-26 17:05:47 +0800

    update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -39,8 +39,8 @@ define('DISALLOW_FILE_MODS',true);
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'localhost');
-define('PATH_CURRENT_SITE', '/menpai/');
+define('DOMAIN_CURRENT_SITE', '/menpai-cc6d1.coding.io');
+define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 
