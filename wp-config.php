86c4eedd7d1748b08d2974b1b937dcdd4ef1df65
gtskk <iamgtskk@gmail.com>
2014-11-29 12:01:59 +0800

    去除系统自带twentytwelve和twentythirteen主题

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'cf_456bf570_fbb5_4cab_b5ea_6c32a92a0e63');
+define('DB_NAME', 'mp');
 
 /** MySQL数据库用户名 */
-define('DB_USER', '2YF7YTOp9qNJDagS');
+define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'xN1Zpn2tlpUEreRz');
+define('DB_PASSWORD', '6399998');
 
 /** MySQL主机 */
-define('DB_HOST', '10.9.1.188');
+define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
@@ -39,8 +39,8 @@ define('DISALLOW_FILE_MODS',true);
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'menpai-cc6d1.coding.io');
-define('PATH_CURRENT_SITE', '/');
+define('DOMAIN_CURRENT_SITE', 'localhost');
+define('PATH_CURRENT_SITE', '/menpai/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 
